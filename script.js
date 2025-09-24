7c64b9c2b6c27054f44be4823264e5d59b06c41e
Hitarth Pathak <163673363+hitarthpathak@users.noreply.github.com>
2025-09-24 15:11:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,77 +1,97 @@
-let userscore = 0;
-let computerscore = 0;
+let user_choices = document.querySelectorAll(".box[id^='user-']");
+let computer_choices = document.querySelectorAll(".box[id^='computer-']");
+let user_score_data = document.querySelector("#user-score");
+let computer_score_data = document.querySelector("#computer-score");
+let result = document.querySelector("#result");
 
 // --------------------------------------------------------------------------------------------------
 
-const choices = document.querySelectorAll(".box");
-const result = document.querySelector("#result");
-
-const userscoredata = document.querySelector("#user-score");
-const computerscoredata = document.querySelector("#computer-score");
+let user_score = 0;
+let computer_score = 0;
 
 // --------------------------------------------------------------------------------------------------
 
-choices.forEach((box) => {
+function show_selection(user_choice, computer_choice) {
+    let user_choice_box = document.getElementById(`user-${user_choice}`);
+    if (user_choice_box) {
+        user_choice_box.style.boxShadow = "0px 0px 20px 10px blue";
+    }
+    let computer_choice_box = document.getElementById(`computer-${computer_choice}`);
+    if (computer_choice_box) {
+        computer_choice_box.style.boxShadow = "0px 0px 20px 10px red";
+    }
+};
+
+// --------------------------------------------------------------------------------------------------
+
+function clear_selection() {
+    user_choices.forEach((box) => {
+        box.style.boxShadow = "none";
+    });
+    computer_choices.forEach((box) => {
+        box.style.boxShadow = "none";
+    });
+};
+
+// --------------------------------------------------------------------------------------------------
+
+user_choices.forEach((box) => {
     box.addEventListener("click", () => {
-        const userchoice = box.getAttribute("id")
-        playgame(userchoice)
-    })
-})
+        let user_choice = box.id.replace("user-", "");
+        clear_selection();
+        play_game(user_choice);
+    });
+});
 
 // --------------------------------------------------------------------------------------------------
 
-const generatecomputerchoice = () => {
-    const options = ["rock", "paper", "scissor"]
-    const randomindex = Math.floor(Math.random() * 3);
+function generate_computer_choice() {
+    let options = ["rock", "paper", "scissor"]
+    let randomindex = Math.floor(Math.random() * 3);
     return options[randomindex];
-}
+};
 
 // --------------------------------------------------------------------------------------------------
 
-const playgame = (userchoice) => {
-    console.log("User Choice =", userchoice)
-    const computerchoice = generatecomputerchoice();
-    console.log("Computer Choice =", computerchoice)
+function play_game(user_choice) {
+    let computer_choice = generate_computer_choice();
 
-    if (userchoice === computerchoice) {
-        // draw game
-        console.log("Draw!")
-        result.innerText = "[ Game Is Draw! ]";
+    show_selection(user_choice, computer_choice);
+
+    if (user_choice === computer_choice) {
+        result.textContent = "[ Game Is Draw! ]";
         result.style.backgroundColor = "white";
     }
     else {
-        let userwin = true;
-        if (userchoice === "rock") {
-            // paper, scissor
-            userwin = computerchoice === "paper" ? false : true;
+        let user_win = true;
+        if (user_choice === "rock") {
+            user_win = computer_choice === "paper" ? false : true;
         }
-        else if (userchoice === "paper") {
-            // rock, scissor
-            userwin = computerchoice === "scissor" ? false : true;
+        else if (user_choice === "paper") {
+            user_win = computer_choice === "scissor" ? false : true;
         }
         else {
-            // rock, paper
-            userwin = computerchoice === "rock" ? false : true;
+            user_win = computer_choice === "rock" ? false : true;
         }
-        showwinner(userwin, userchoice, computerchoice);
+        show_winner(user_win, user_choice, computer_choice);
     }
-}
+};
 
 // --------------------------------------------------------------------------------------------------
 
-const showwinner = (userwin, userchoice, computerchoice) => {
-    if (userwin) {
-        console.log("You Win!")
-        userscore++;
-        userscoredata.innerText = userscore;
-        result.innerText = `[ You Win! Your ${userchoice} beats Computer's ${computerchoice} ]`;
+function show_winner(user_win, user_choice, computer_choice) {
+    let show_user_choice = user_choice.toUpperCase();
+    let show_computer_choice = computer_choice.toUpperCase();
+    if (user_win) {
+        user_score++;
+        user_score_data.textContent = user_score;
+        result.textContent = `[ You Win! Your ${show_user_choice} beats Computer's ${show_computer_choice} ]`;
         result.style.backgroundColor = "green";
     }
     else {
-        console.log("You Loose!")
-        computerscore++;
-        computerscoredata.innerText = computerscore;
-        result.innerText = `[ You Loose! Computer's ${computerchoice} beats Your ${userchoice} ]`;
+        computer_score++;
+        computer_score_data.textContent = computer_score;
+        result.textContent = `[ You Loose! Computer's ${show_computer_choice} beats Your ${show_user_choice} ]`;
         result.style.backgroundColor = "red";
     }
-}
+};
